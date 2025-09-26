f982308df728c75b913176b9073435d0d1f81f66
easyaneasy <easy.an.easy@gmail.com>
2025-09-26 22:37:24 +0900

    react JSX 문법과 컴포넌트 실습

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      {/* <div>
+        <h1>Hello World</h1>
+      </div> */}
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
